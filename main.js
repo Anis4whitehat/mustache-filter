d96549d9d1a6f40a5a51d58ac3bae0e03de2fe08
Anis4whitehat <82126047+Anis4whitehat@users.noreply.github.com>
2021-12-03 19:46:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
+    mustache = loadImage('https://i.postimg.cc/4Nb6gWXs/better-mustache.png');
 }
 
 function setup(){
@@ -7,12 +10,29 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY,  80, 35);
 }
 
 function take_snapshot(){
     save('mustache.png')
+}
+
+function modelLoaded(){
+    console.log('poseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x -40;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
